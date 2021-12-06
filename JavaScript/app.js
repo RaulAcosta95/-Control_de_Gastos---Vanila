9c2e35bc9593d4e7694c04e1eb9cce05e5197e6c
Raúl <raulacostaservicio@hotmail.com>
2021-12-06 08:52:38 -0600

    Service worker desactivado

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,15 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./ServiceWorker.js')
-    .then((reg) => console.log('Service Worker Registered', reg))
-    .catch((err) => console.log('Service Worker NOT Registered', err));
+// if('serviceWorker' in navigator){
+//     navigator.serviceWorker.register('./ServiceWorker.js')
+//     .then((reg) => console.log('Service Worker Registered', reg))
+//     .catch((err) => console.log('Service Worker NOT Registered', err));
+// }
+
+if(navigator.onLine) {
+    console.log('Es online');
+} else {
+    setTimeout(() => {
+        console.log('Es Offline');
+        document.location.href="fallback.html";
+    }, 1000);
+
 }
