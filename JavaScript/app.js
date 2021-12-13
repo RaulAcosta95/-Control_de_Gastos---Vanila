66dbb3c50fda0b97fcf5c1d344f3eefe25586a85
Raúl <raulacostaservicio@hotmail.com>
2021-12-13 17:22:14 -0600

    service worker registered

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,15 @@
-// if('serviceWorker' in navigator){
-//     navigator.serviceWorker.register('./ServiceWorker.js')
-//     .then((reg) => console.log('Service Worker Registered', reg))
-//     .catch((err) => console.log('Service Worker NOT Registered', err));
-// }
-
-if(navigator.onLine) {
-    console.log('Es online');
-} else {
-    setTimeout(() => {
-        console.log('Es Offline');
-        document.location.href="fallback.html";
-    }, 1000);
-
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('./ServiceWorker.js')
+    .then((reg) => console.log('Service Worker Registered', reg))
+    .catch((err) => console.log('Service Worker NOT Registered', err));
 }
+
+// if(navigator.onLine) {
+//     console.log('Es online');
+// } else {
+//     setTimeout(() => {
+//         console.log('Es Offline');
+//         document.location.href="fallback.html";
+//     }, 1000);
+
+// }
